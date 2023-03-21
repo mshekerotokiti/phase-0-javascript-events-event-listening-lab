694765e94de3e6a575814b78b8d702fb00c86928
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-21 07:18:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ document.getElementById('button'). addEventListener('click', () => alert ('I was
 }
 
 console.log(addingEventListener());
+
+/*const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+input.addEventListener('click', clickAlert);*/
+
+document.getElementsByClassName('eventy').addEventListener('form', () => prompt ('To do list'))
